570453e84d718ba7c01eca3c59062f5137d9c5fe
alezp <polli.alejandro89@gmail.com>
2024-10-29 17:08:02 -0300

    Primera Entidad

---
+++ LABA/Laba.Backend/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+public class DataContext : DbContext
+        {
+            public DataContext(DbContextOptions<DataContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Vehiculo> Vehiculos { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.Entity<Vehiculo>().HasIndex(c => c.Name).IsUnique();
+            }
+        }
+    }
+
+}
+}
